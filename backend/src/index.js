a8c54c496c585ce807f437ce21f26603a115e758
priyanshuraj27 <priyanshuraj1250@gmail.com>
2025-04-04 17:57:39 +0530

    feat: update package.json and package-lock.json with new dependencies; add app.js and refactor index.js for server setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,2 +1,17 @@
-require("dotenv").config();
-console.log("Hello from the backend!");
+import dotenv from 'dotenv';
+import connectDB from './db/index.js';
+import { app } from './app.js';
+dotenv.config({
+    path: "./.env"
+});
+
+
+connectDB()
+    .then(() => {
+        app.listen(process.env.PORT || 8000, () => {
+            console.log(`Server is running on port ${process.env.PORT || 8000}`);
+        });
+    })
+    .catch((error) => {
+        console.log("MONGO DB connection failed ", error);
+    });
